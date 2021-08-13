d7be08cb36c44cd94e7924bd53203c267058580a
Erzhan2101 <erzhan210198@gmail.com>
2021-08-13 21:50:48 +0600

    new'

--- script.js
+++ script.js
@@ -382,5 +382,13 @@
 
 // task -- Last Survivor
 function lastSurvivor(letters, coords) {
-
+let arr = letters.split('')
+    for (let i = 0; i < coords.length; i++){
+        arr.splice(coords[i], 1)
+    }
+    return arr.join('')
 }
+
+// hi insts: erjan2101
+
+console.log(lastSurvivor('hgf', [0, 1]))
